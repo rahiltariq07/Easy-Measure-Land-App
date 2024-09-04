d504bc09097c4df7f3078d69429eb744437f7ade
Rahil Tariq <163430192+rahiltariq07@users.noreply.github.com>
2024-09-04 09:56:09 +0530

    Add files via upload

---
+++ Easy Measure/home.js/script.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    document.querySelector('.navbar').classList.toggle('active');
+}
+
+document.getElementById('start-measuring').addEventListener('click', () => {
+    window.location.href = './start/start.html'
+});
+
+document.getElementById('get-started').addEventListener('click', () => {
+    window.location.href = './start/start.html'
+});
+
